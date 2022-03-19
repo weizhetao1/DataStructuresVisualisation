fb02fb3694194f8ad6c82ca91866c04b5ed3300b
William Tao <tao.w@etoncollege.org.uk>
2022-03-19 10:44:52 +0000

    Initial Commit

---
+++ DataStructuresVisualisation/DataStructuresVisualisationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresVisualisationApp.swift
+//  DataStructuresVisualisation
+//
+//  Created by Tao, Weizhe (Coll) on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresVisualisationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
